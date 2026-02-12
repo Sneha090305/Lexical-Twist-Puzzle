6ae751e8179cbfd213301b0cbd7a7a576f7c8420
Sneha <snehangap@gmail.com>
2026-02-12 17:07:16 +0530

    [Sneha]: Added: UC3: Implement single-word validation

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -11,8 +11,20 @@ public class LexicalTwistPuzzle {
         System.out.print("Enter first word: ");
         String firstWord = scanner.nextLine();
 
+        // Validation for first word
+        if (firstWord.contains(" ")) {
+            System.out.println(firstWord + " is an invalid word");
+            return; // graceful termination
+        }
+
         System.out.print("Enter second word: ");
         String secondWord = scanner.nextLine();
+
+        // Validation for second word
+        if (secondWord.contains(" ")) {
+            System.out.println(secondWord + " is an invalid word");
+            return; // graceful termination
+        }
 
         scanner.close();
     }
